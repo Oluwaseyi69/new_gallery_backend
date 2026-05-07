b231141fe382a893a8c1f899e5f709889777252e
Oluwaseyi69 <oluwaseyitemitope69@gmail.com>
2026-05-07 10:16:24 +0100

    Fix Image Entity

--- src/main/java/com/imagesave/imagesave/data/models/Image.java
+++ src/main/java/com/imagesave/imagesave/data/models/Image.java
@@ -4,6 +4,7 @@ package com.imagesave.imagesave.data.models;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Setter
@@ -11,6 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document("Images")
 public class Image {
 
+    @Id
     private String id;
     private String name;
     private String link;
